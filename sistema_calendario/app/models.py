70239e006099e82eef21a012db0aa2985a9baf43
victorso <v+github@victor.so>
2015-11-22 19:51:56 -0200

    organização

--- sistema_calendario/app/models.py
+++
@@ -1,9 +0,0 @@
-from app import db
-class Post(db.Model):
-  id = db.Column(db.Integer, primary_key=True)
-  title = db.Column(db.String(128))
-  body = db.Column(db.Text)
-
-  def __init__(self, title, body):
-        self.title = title
-        self.body = body
--- sistema_calendario/wsgi.py
+++ sistema_calendario/wsgi.py
@@ -1,4 +1,6 @@
-from sistema_calendario import application
+import sys
+sys.path.insert(0, '/srv/sistema_calendario/sistema_calendario_app')
+from sistema_calendario_app.app import app as application
 
 if __name__ == "__main__":
     application.run()
